e96c872450e4d0b544946a504053b6f24c1d9301
Artyom <mememail@rambler.ru>
2020-11-18 17:10:37 +0300

    comment

--- program.py
+++ program.py
@@ -8,3 +8,7 @@ def hello_world():
 @app.route('/v2')
 def v2():
     return 'Second action'
+
+@app.route('/Minachev')
+def Minachev():
+    return 'Hello from CI with GitHub Actions by Minachev'
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,3 +10,8 @@ def test_v2(app, client):
     res = client.get('/v2')
     assert res.status_code == 200
     assert res.data == b"Second action"
+
+def test_Minachev(app, client):
+    res = client.get('/Minachev')
+    assert res.status_code == 200
+    assert res.data == b"Hello from CI with GitHub Actions by Minachev"
